88465dab9c79810e99a65ccdbc5e2e071780867f
T15521 <tpasinato@suppliers.tenaris.com>
2020-06-27 20:47:35 -0300

    commit inicial

--- app.py
+++ app.py
@@ -8,7 +8,12 @@ class Hola(Resource):
     def get(self, name):
         return {"Hello":name}
 
+class Hola2(Resource):
+    def get(self):
+        return {"HOLASA":"HOLASA"}
+
 api.add_resource(Hola, '/hola/<name>')
+api.add_resource(Hola2, '/hola2')
 
 if __name__ == '__main__':
      app.run(debug=True)
