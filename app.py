8a2ae410ca2066d9742927c9139966bf45bc71a5
T15521 <tpasinato@suppliers.tenaris.com>
2020-06-28 22:34:07 -0300

    agrego DB

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
 api = Api(app)
+
+from models import Book
 
 class Hola(Resource):
     def get(self, name):
