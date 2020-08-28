31a19872b73a808fa6bb1756043f0001bc1d9251
T15521 <tpasinato@suppliers.tenaris.com>
2020-08-27 22:08:13 -0300

    Cambio en models tab por espacios

--- models.py
+++ models.py
@@ -7,13 +7,13 @@ class Receta(db.Model):
     nombre = db.Column(db.String())
     calificacion = db.Column(db.Integer)
     tiempoPreparacion = db.Column(db.Integer)
-	nuevo = db.Column(db.Integer)
+    nuevo = db.Column(db.Integer)
 
     def __init__(self, nombre, calificacion, tiempoPreparacion, nuevo):
         self.nombre = nombre
         self.calificacion = calificacion
         self.tiempoPreparacion = tiempoPreparacion
-		self.nuevo = nuevo
+        self.nuevo = nuevo
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
@@ -24,5 +24,5 @@ class Receta(db.Model):
             'nombre': self.nombre,
             'calificacion': self.calificacion,
             'tiempoPreparacion':self.tiempoPreparacion
-			'nuevo':self.nuevo
+            'nuevo':self.nuevo
 }
