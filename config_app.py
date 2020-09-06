875ea04220ec842bf30318a5c32ac61e6fe58c22
evesacks <evesacks98@gmail.com>
2020-09-06 19:53:38 -0300

    Configuracion de la base de datos

--- config_app.py
+++ config_app.py
@@ -5,8 +5,8 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SECRET_KEY = '677a6637137a00c8c3aed7246b14d0e1d4eda1bfa4270a2f390580ddc3c3879d'
+    SQLALCHEMY_DATABASE_URI = os.environ['heroku pg:psql postgresql-reticulated-37565 --app celiakia20']
 
 
 class ProductionConfig(Config):
