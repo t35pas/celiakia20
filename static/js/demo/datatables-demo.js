0861ab7af1d4e9ad23f3b2f044a13500278d8582
Evelyn <evesacks98@gmail.com>
2020-12-15 18:20:55 -0300

    .

--- static/js/demo/datatables-demo.js
+++ static/js/demo/datatables-demo.js
@@ -1,4 +1,24 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  $('#dataTable').DataTable({
+    responsive: true,
+    language: {
+        "processing": "Procesando...",
+        "lengthMenu": "Mostrar _MENU_ ",
+        "zeroRecords": "No se encontraron resultados",
+        "emptyTable": "Ninguna resultado disponible en esta tabla",
+        "info": "Mostrando del _START_ al _END_ de un total de _TOTAL_ ",
+        "infoEmpty": "Mostrando del 0 al 0 de un total de 0 ",
+        "infoFiltered": "(filtrado de un total de _MAX_ )",
+        "search": "Buscar:",
+        "infoThousands": ",",
+        "loadingRecords": "Cargando...",
+        "paginate": {
+            "first": "Primero",
+            "last": "Último",
+            "next": "Siguiente",
+            "previous": "Anterior"
+        }
+    } 
+  })
 });
