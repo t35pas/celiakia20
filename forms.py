a6d0a29c91c8e4b52c9edaccab031381e2e02be4
Evelyn <you@example.com>
2022-03-12 18:11:29 -0300

    correccion llamada a models

--- forms.py
+++ forms.py
@@ -3,206 +3,82 @@ from wtforms import StringField, PasswordField, SubmitField, IntegerField, Selec
 from wtforms.fields.core import BooleanField
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
-from models import Administrador, Ingrediente, Preparacion, Receta, Ingrediente_Por_Receta, Unidad, Dificultad
+from models.administrador import Administrador
+from models.difciultad import Dificultad
+from models.favorito import Favorito
+from models.ingrediente import Ingrediente
+from models.ingrediente_por_receta import Ingrediente_Por_Receta
+from models.preparacion import Preparacion
+from models.receta import Receta
+from models.unidad import Unidad
+from models.usuario import Usuario
 
 class LoginForm(FlaskForm):
-    nombreUsuario = StringField(
-                        'Nombre de usuario', 
-                        validators=[
-                            DataRequired(), 
-                            Length(min=2, max=20)
-                        ]
-                    )
-    contrasenia = PasswordField(
-                    'Contrasenia',
-                    validators=[
-                        DataRequired()
-                    ]
-                )
+    nombreUsuario = StringField('Nombre de usuario', validators=[DataRequired(), Length(min=2, max=20)])
+    contrasenia = PasswordField('Contrasenia',validators=[DataRequired()])
     submit = SubmitField('Ingresar')
 
 class NuevaReceta(FlaskForm):
-    tituloReceta = StringField(
-                    'Titulo de la receta', 
-                    validators=[
-                        DataRequired(), 
-                        Length(min=2, max=50)
-                    ]
-                )
-    tiempoPreparacion = IntegerField(
-                            'Tiempo de preparación', 
-                            validators=[
-                                DataRequired()
-                            ]
-                        )
-    dificultad = SelectField(
-                    'Dificultad',
-                    choices=[], 
-                    coerce=int
-                )
-    imagenReceta = FileField(
-                    'Imagen de la receta',
-                    validators=[
-                        FileRequired(),
-                        FileAllowed(['jpg', 'png'])
-                    ]
-                )
-    nombreImagen = StringField(
-                    'Nombre de la imagen', 
-                    validators=[
-                        DataRequired(), 
-                        Length(min=2, max=20),
-                    ]
-                )
+    tituloReceta = StringField('Titulo de la receta', validators=[DataRequired(), Length(min=2, max=50)])
+    tiempoPreparacion = IntegerField('Tiempo de preparación', validators=[DataRequired()])
+    dificultad = SelectField('Dificultad',choices=[], coerce=int)
+    imagenReceta = FileField('Imagen de la receta',validators=[FileRequired(),FileAllowed(['jpg', 'png'])])
+    nombreImagen = StringField('Nombre de la imagen', validators=[DataRequired(), Length(min=2, max=20),])
     submit = SubmitField('Siguiente')
 
     def validate_nombreImagen(self, nombreImagen):
-        receta = Receta.query.filter_by(nombre_imagen = nombreImagen.data).first()
+        receta = Receta.find_by_file(nombreImagen.data)
         if receta:
             raise ValidationError('Ya existe ese nombre de imagen!')
     
     def validate_tituloReceta(self, tituloReceta):
-        receta = Receta.query.filter_by(titulo = tituloReceta.data).first()
+        receta = Receta.find_by_name(tituloReceta.data)
         if receta:
             raise ValidationError('Ya existe ese Título!')
 
-class EditarInfoGral(FlaskForm):
-    
-    tituloReceta = StringField(
-                    'Titulo de la receta', 
-                    id="editarTitulo",
-                    validators=[
-                        DataRequired(), 
-                        Length(min=2, max=50)
-                    ]
-                )
-    tiempoPreparacion = IntegerField(
-                    'Tiempo de preparación', 
-                    id="editarTiempoPreparacion",
-                    validators=[
-                        DataRequired()
-                    ]
-                        )
-    dificultad = SelectField(
-                    'Dificultad',
-                    id="editarDificultad",
-                    choices=[(dif.id, dif.descripcion) for dif in Dificultad.query.all()], 
-                    coerce=int
-                )
-    imagenReceta = FileField(
-                    'Imagen de la receta',
-                    id="editarImagenReceta",
-                    validators=[
-                        FileAllowed(['jpg', 'png'])
-                    ]
-                )
-    nombreImagen = StringField(
-                    'Nombre de la imagen',
-                    id="editarNombreImagenReceta", 
-                    validators=[
-                        DataRequired(), 
-                        Length(min=2, max=20),
-                    ]
-                )
-    submit = SubmitField('Actualizar')
-
 class NuevaPreparacion(FlaskForm):
-    ordenPaso = IntegerField(
-                    'Orden', 
-                    validators=[
-                        DataRequired()
-                    ]
-                )
-    descripcion = TextAreaField(
-                    'Descripción',
-                    validators=[
-                        DataRequired()
-                    ]
-                )
+    ordenPaso = IntegerField('Orden', validators=[DataRequired()])
+    descripcion = TextAreaField('Descripción',validators=[DataRequired()])
+    tiempo = IntegerField('Tiempo aprox del paso', validators=[DataRequired()])
     submit = SubmitField('Agregar')
 
+class NuevoIngrediente(FlaskForm):
+
+    descripcionIngrediente = SelectField('Nombre del ingrediente',id = 'descripcionIngrediente',choices=[],coerce=int)
+    unidad = SelectField('Unidad',choices=[],coerce=int)
+    cantidad = IntegerField('Cantidad',validators=[DataRequired()])
+    submit = SubmitField('Agregar')
+
+
+class EditarInfoGral(FlaskForm):
+    
+    tituloReceta = StringField('Titulo de la receta', id="editarTitulo",validators=[DataRequired(), Length(min=2, max=50)])
+    tiempoPreparacion = IntegerField('Tiempo de preparación', id="editarTiempoPreparacion",validators=[DataRequired()])
+    dificultad = SelectField('Dificultad',id="editarDificultad",choices=[(dif.id, dif.descripcion) for dif in Dificultad.query.all()], coerce=int)
+    imagenReceta = FileField('Imagen de la receta',id="editarImagenReceta",validators=[FileAllowed(['jpg', 'png'])])
+    nombreImagen = StringField('Nombre de la imagen',id="editarNombreImagenReceta", validators=[DataRequired(), Length(min=2, max=20),])
+    submit = SubmitField('Actualizar')
+
+
+
 class EditarPreparacion(FlaskForm):
-    ordenPaso = IntegerField(
-                    'Orden', 
-                    id = 'editarOrdenPaso',
-                    validators=[
-                        DataRequired()
-                    ]
-                )
-    descripcion = TextAreaField(
-                    'Descripción',
-                    id = 'editarDescripcion',
-                    validators=[
-                        DataRequired()
-                    ]
-                )
-    submit = SubmitField('Actualizar',
-                    id = 'actualizar')
+    ordenPaso = IntegerField('Orden', id = 'editarOrdenPaso',validators=[DataRequired()])
+    descripcion = TextAreaField('Descripción',id = 'editarDescripcion',validators=[DataRequired()])
+    submit = SubmitField('Actualizar',id = 'actualizar')
 
 class CambioContraseniaAdmin(FlaskForm):
-    nombreUsuario = StringField(
-                        'Nombre de usuario', 
-                        validators=[
-                            DataRequired(), 
-                            Length(min=2, max=20)
-                        ]
-                    )
-    contraseniaAntigua = PasswordField(
-                            'Antigua contraseña', 
-                            validators=[
-                                DataRequired()
-                            ]
-                        )
-    contraseniaNueva = PasswordField(
-                        'Nueva contraseña', 
-                        validators=[
-                            DataRequired()
-                        ]
-                    )
-    repetirContraseniaNueva = PasswordField(
-                                'Repetir nueva contraseña', 
-                                validators=[
-                                    DataRequired(),
-                                    EqualTo('contraseniaNueva')
-                                ]
-                            )
+    nombreUsuario = StringField('Nombre de usuario', validators=[DataRequired(), Length(min=2, max=20)])
+    contraseniaAntigua = PasswordField('Antigua contraseña', validators=[DataRequired()])
+    contraseniaNueva = PasswordField('Nueva contraseña', validators=[DataRequired()])
+    repetirContraseniaNueva = PasswordField('Repetir nueva contraseña', validators=[DataRequired(),EqualTo('contraseniaNueva')])
     submit = SubmitField('Cambiar contraseña')
 
 class NuevoAdmin(FlaskForm):
-    nombreApellido = StringField(
-                'Nombre y apellido', 
-                validators=[
-                    DataRequired(), 
-                    Length(min=2, max=100)
-                ]
-            )
-    nombreUsuario = StringField(
-                        'Nombre de usuario', 
-                        validators=[
-                            DataRequired(), 
-                            Length(min=2, max=20)
-                        ]
-                    )
-    email = StringField(
-                'E-mail', 
-                validators=[
-                    DataRequired(),
-                    Email()
-                ]
-            )
-    contrasenia = PasswordField(
-                    'Contraseña', 
-                    validators=[
-                        DataRequired()
-                    ]
-                )
-    repetirContrasenia = PasswordField(
-                            'Repetir contraseña', 
-                            validators=[
-                                DataRequired(),
-                                EqualTo('contraseniaNueva')
-                            ]
-                        )
+    nombreApellido = StringField('Nombre y apellido', validators=[DataRequired(), Length(min=2, max=100)])
+    nombreUsuario = StringField('Nombre de usuario', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('E-mail', validators=[DataRequired(),Email()])
+    contrasenia = PasswordField('Contraseña', validators=[DataRequired()])
+    repetirContrasenia = PasswordField('Repetir contraseña', validators=[DataRequired(),EqualTo('contraseniaNueva')])
     superAdmin = BooleanField('Super Administrador')
     submit = SubmitField('Cambiar contraseña')
     
@@ -217,13 +93,7 @@ class NuevoAdmin(FlaskForm):
             raise ValidationError('Ese E-mail ya esta registrado!')
         
 class NuevaUnidad(FlaskForm):
-    nombreUnidad = StringField(
-                    'Nombre de la unidad', 
-                    validators=[
-                        DataRequired(), 
-                        Length(min=2, max=50)
-                    ]
-                )
+    nombreUnidad = StringField('Nombre de la unidad', validators=[DataRequired(), Length(min=2, max=50)])
     submit = SubmitField('Agregar')
 
     def validate_nombreUnidad(self, nombreUnidad):
@@ -231,85 +101,12 @@ class NuevaUnidad(FlaskForm):
         if unidad:
             raise ValidationError('Ya existe esa unidad!')
 
-class NuevoIngrediente(FlaskForm):
-
-    nombreIngrediente = StringField(
-                            'Nombre del ingrediente',
-                            id="nombreIngrediente"
-                        )
-    elegirNombreIngrediente = SelectField(
-                            'Nombre del ingrediente',
-                            id = 'elegirNombreIngrediente',
-                            choices=[],
-                            coerce=int
-                        )
-    unidad = SelectField(
-                'Unidad',
-                choices=[],
-                coerce=int
-            )
-    cantidad = IntegerField(
-                'Cantidad',
-                validators=[
-                    DataRequired()
-                ]
-            )
-    imagenIngrediente = FileField(
-                    'Imagen del Ingrediente',
-                    validators=[
-                        FileAllowed(['jpg', 'png'])
-                    ]
-                )
-    nombreImagen = StringField(
-                    'Nombre de la imagen'
-                )
-    submit = SubmitField('Agregar')
-
-    def validate_nombreImagen(self, nombreImagen):
-        ingrediente = Ingrediente.query.filter_by(nombre_imagen = nombreImagen.data).first()
-        if ingrediente:
-            raise ValidationError('Ya existe ese nombre de imagen!')
-    def validate_nombreIngrediente_unidad(self, nombreIngrediente, unidad):
-        ingredientes = Ingrediente.query.filter_by(descripcion = nombreIngrediente.data)
-        for ingrediente in ingredientes:
-            if ingrediente.unidad == unidad.data:
-                raise ValidationError('Ya existe ese ingrediente!')
 
 class EditarIngrediente(FlaskForm):
-    nombreIngrediente = StringField(
-                        'Nombre del ingrediente',
-                        id = 'editarNombreIngrediente',
-                        validators=[
-                            DataRequired(), 
-                            Length(min=2, max=50)
-                        ]
-                    )
-    unidad = SelectField(
-                'Unidad',
-                id = 'editarUnidad',
-                choices=[(u.id, u.descripcion) for u in Unidad.query.all()], 
-                coerce=int
-            )
-    cantidad = IntegerField(
-                'Cantidad',
-                id = 'editarCantidad',
-                validators=[
-                    DataRequired()
-                ]
-            )
-    imagenIngrediente = FileField(
-                    'Imagen del Ingrediente',
-                    id = 'editarImagenIngrediente',
-                    validators=[
-                        FileAllowed(['jpg', 'png'])
-                    ]
-                )
-    nombreImagen = StringField(
-                    'Nombre de la imagen',
-                    id = 'editarNombreImagenIng', 
-                    validators=[
-                        Length(min=2, max=20)
-                    ]
-                )
+    nombreIngrediente = StringField('Nombre del ingrediente',id = 'editarNombreIngrediente',validators=[DataRequired(), Length(min=2, max=50)])
+    unidad = SelectField('Unidad',id = 'editarUnidad',choices=[(u.id, u.descripcion) for u in Unidad.query.all()], coerce=int)
+    cantidad = IntegerField('Cantidad',id = 'editarCantidad',validators=[DataRequired()])
+    imagenIngrediente = FileField('Imagen del Ingrediente',id = 'editarImagenIngrediente',validators=[FileAllowed(['jpg', 'png'])])
+    nombreImagen = StringField('Nombre de la imagen',id = 'editarNombreImagenIng', validators=[Length(min=2, max=20)])
     submit = SubmitField('Actualizar')
 
