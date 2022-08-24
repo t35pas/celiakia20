ddabc423a02a544b67036c123f1adaf428a3acde
Timoteo Pasinato <timoteopasinato@gmail.com>
2022-08-24 20:38:04 -0300

    enviar imagen a receta

--- app.py
+++ app.py
@@ -471,7 +471,7 @@ def RecetasPorIngrediente():
 
 @app.route('/ObtenerImagen/<nombre>')
 def ObtenerImagen(nombre):
-        filename = 'imagenes/'+ nombre
+        filename = '/imagenes/'+ nombre
         return send_file(filename, mimetype='image/jpg')
 
 @app.route('/verReceta/<idReceta>', methods = ['GET', 'POST'])
