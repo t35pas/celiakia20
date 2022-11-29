6d6fb87cf458fa52dcf8e17d998555c42ace0ad7
= <=>
2022-11-28 23:54:24 -0300

    test ObtenerImagen

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['UPLOAD_FOLDER'] = '/app/static/imagenes/'
+UPLOAD_FOLDER = '/app/static/imagenes/'
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=60)
 ROWS_PER_PAGE = 5
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
@@ -67,7 +67,7 @@ def guardar_imagen(nombre, imagen):
                 nombreImagen = nombre + extension
                 print("Nombre imagen con la extension",nombreImagen)
                 filename = secure_filename(nombreImagen)
-                imagen.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                imagen.save(UPLOAD_FOLDER + filename)
         return filename
 
 def renombrar_imagen(nombreActual, nombreNuevo):
@@ -296,7 +296,7 @@ def MundoCeliakia():
 
 @app.route('/ObtenerImagen/<nombre>')
 def ObtenerImagen(nombre):
-        return send_from_directory(app.config["UPLOAD_FOLDER"], nombre)
+        return send_from_directory(UPLOAD_FOLDER, nombre)
 
 @app.route('/verReceta/<idReceta>', methods = ['GET', 'POST'])
 @login_required
